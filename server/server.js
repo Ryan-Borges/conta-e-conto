d81bb23b0c554cdcb8181d6e44de8d445c8719b9
Ryan Borges <ryan.cunha.borges@gmail.com>
2026-08-23 21:00:24 -0300

    Restringe CORS para producao

--- server/server.js
+++ server/server.js
@@ -17,7 +17,28 @@ const PORT =
 // CONFIGURAÇÕES
 // ==========================================
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "https://conta-e-conto.vercel.app",
+            "http://127.0.0.1:5500",
+            "http://localhost:5500"
+        ],
+
+        methods: [
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ],
+
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization"
+        ]
+    })
+);
 
 app.use(express.json());
 
